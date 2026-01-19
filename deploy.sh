7e2b580b42b07190c512ca8766c8fd10c54c3278
Kevin Houdebert <kevin.houdebert@gmail.com>
2026-01-19 02:02:23 +0100

    feat: Add deployment script for automated container management and Cloudflare cache purging

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ~/docker/manaforge
+
+echo "### Git pull"
+git pull
+
+echo "### Rebuilding and restarting containers"
+docker compose up -d --build
+echo "### Updating static files and restarting nginx"
+docker cp manaforge-api-1:/app/app/static/dist ./app/static/
+docker compose restart nginx
+
+echo "### Purging Cloudflare cache"
+source .env
+curl -XPOST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" -H "Content-Type: application/json" -d '{"hosts":["manaforge.houke.fr"]}'
+echo
+echo "### Deployment complete"
